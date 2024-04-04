098d92f5fcb5fa4176d120c371917df44093d096
marirs <251870+marirs@users.noreply.github.com>
2024-04-04 13:55:37 +0530

    update

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
-use std::fs::File;
-use std::process::exit;
+use clap::Parser;
+use log::{debug, info};
 use mongodb::bson::doc;
 use mongodb::Client;
 use serde::{Deserialize, Serialize};
-use clap::Parser;
-use log::{debug, info};
+use std::fs::File;
+use std::process::exit;
 use tokio::spawn;
-use crate::config::Config;
 
-mod config;
 mod error;
 
 type Result<T> = std::result::Result<T, error::Error>;
+
+/// Mongo URL
+const MONGODB_URL: &str = "mongodb://localhost:27017/whois";
+/// Mongo Database
+const MONGODB_DB: &str = "whois";
+/// Mongo Collection
+const MONGODB_COLLECTION: &str = "feeds";
 
 #[derive(Clone, Debug, Serialize, Deserialize, Default)]
 pub struct WhoIsRecord {
@@ -36,69 +41,60 @@ pub struct WhoIsRecord {
     pub registrant_phone: Option<String>,
     pub registrant_fax: Option<String>,
     pub registrant_email: Option<String>,
-    pub name_servers: Option<String>
+    pub name_servers: Option<String>,
 }
 
 #[derive(Clone, Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct CliOpts {
-    /// The path to the configuration file.
-    #[clap(short, long)]
-    pub config: String,
+    /// The path to the CSV files.
+    #[arg(
+        short = 'f',
+        long,
+        value_name = "CSV-FILES-PATH",
+        default_value = "./data"
+    )]
+    pub csv_files_path: String,
 }
 
 #[tokio::main]
 async fn main() {
     let opts: CliOpts = CliOpts::parse();
     simple_logger::init_with_level(log::Level::Debug).ok();
-    let config = match Config::from_file(&opts.config) {
-        Ok(config) => config,
-        Err(e) => {
-            eprintln!("Error reading config file: {:?}", e);
-            exit(1);
-        }
-    };
-    if let Err(e) = read_directory(&config).await {
+    info!("Reading the directory: {:?}", opts.csv_files_path);
+    if let Err(e) = read_directory(&opts.csv_files_path).await {
         eprintln!("Error reading the directory: {:?}", e);
         exit(1);
     }
     info!("Successfully read the directory.");
 }
 
-
-pub async fn read_directory(config: &Config) -> Result<()> {
+pub async fn read_directory(source_folder: &str) -> Result<()> {
     //! Read all the csv files in the directory and parse the csv content into whois records. The
     //! records are then saved to a MongoDB database.
-    let directory_path = &config.data_path;
-    let connection_string = format!("mongodb://{}:{}@{}:{}",
-                                    config.db.username,
-                                    config.db.password,
-                                    config.db.host,
-                                    config.db.port);
-    let client = Client::with_uri_str(&connection_string).await?;
-    let paths = std::fs::read_dir(directory_path)?;
+    let mongo_client = Client::with_uri_str(MONGODB_URL).await?;
+    let paths = std::fs::read_dir(source_folder)?;
     for path in paths {
         // TODO: Check if the file is a csv file
-        let config = config.clone();
-        let client = client.clone();
+        let mongo_client = mongo_client.clone();
         // Run in parallel
         spawn(async move {
-            let config = config.clone();
             let path = path.unwrap().path();
             info!("Processing file: {:?}", path);
             if path.is_file() {
                 let file_path = path.to_str().unwrap();
-                let client_ref = client.clone();
-                let file_records = read_file(file_path).unwrap();
+                let mongo_client_ref = mongo_client.clone();
+                let racords = read_file(file_path).unwrap();
                 // save the records
-                save_to_db(client_ref, &config, file_records).await.unwrap();
+                save_to_db(mongo_client_ref, racords).await.unwrap();
             }
-        }).await?;
+        })
+        .await?;
     }
     Ok(())
 }
 
-pub fn read_file(file_path: &str) -> Result<Vec<WhoIsRecord>>{
+pub fn read_file(file_path: &str) -> Result<Vec<WhoIsRecord>> {
     //! Read the file asynchronously and parse the csv content into a whois record.
     let file = File::open(file_path)?;
     let mut reader = csv::Reader::from_reader(file);
@@ -112,21 +108,21 @@ pub fn read_file(file_path: &str) -> Result<Vec<WhoIsRecord>>{
     Ok(records)
 }
 
-pub async fn save_to_db(client: Client, config: &Config, records: Vec<WhoIsRecord>) -> Result<()> {
+pub async fn save_to_db(client: Client, records: Vec<WhoIsRecord>) -> Result<()> {
     //! Save the `whois` records to a MongoDB database. The database name and collection name is
-    //! read from the config file. This function basically insert the records into the collection but 
+    //! read from the config file. This function basically insert the records into the collection but
     //! if the record already exists in the collection, it updates the record.
-    let db = client.database(&config.db.database);
-    let collection = db.collection::<WhoIsRecord>(&config.db.collection);
+    let db = client.database(MONGODB_DB);
+    let collection = db.collection::<WhoIsRecord>(MONGODB_COLLECTION);
     info!("Saving records to the database. This may take a while...");
     let mut join_handles = Vec::new();
     for record in records {
         debug!("Saving record number - {}...", record.num);
-        // Use a thread pool to quickly and efficiently save the records. 
+        // Use a thread pool to quickly and efficiently save the records.
         let collection = collection.clone();
         join_handles.push(spawn(async move {
             // Check if the record exists in the db, if it does update else insert.
-            
+
             let filter = doc! {
                 "domain_name": &record.domain_name
             };
@@ -154,7 +150,9 @@ pub async fn save_to_db(client: Client, config: &Config, records: Vec<WhoIsRecor
                     "name_servers": &record.name_servers
                 }
             };
-            let options = mongodb::options::UpdateOptions::builder().upsert(true).build();
+            let options = mongodb::options::UpdateOptions::builder()
+                .upsert(true)
+                .build();
             collection.update_one(filter, update, options).await.ok();
         }));
     }
