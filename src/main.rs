65de900dffa8793dcfd33aa8ce6dfbaa59fc2443
marirs <251870+marirs@users.noreply.github.com>
2024-04-11 11:58:23 +0530

    update

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ use std::{process::exit, time::Instant};
 use tokio::spawn;
 use whois::WhoIsRecord;
 
-#[cfg(not(target_env = "msvc"))]
+#[cfg(not(target_os = "windows"))]
 use jemallocator::Jemalloc;
 
-#[cfg(not(target_env = "msvc"))]
+#[cfg(not(target_os = "windows"))]
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
 
